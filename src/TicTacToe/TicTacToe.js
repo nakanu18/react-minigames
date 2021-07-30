9e92706757704d8d4d524197c3d39211158dca96
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-07-30 07:13:44 -0500

    onPress buttons

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -12,30 +12,33 @@ export default class TicTacToe extends Component {
                 ["-", "-", "-"],
             ],
         };
+    onPress = buttonId => {
+        console.log(hello);
+    }
     }
 
     render() {
         return (
             <>
                 <div>
+                    <h1 className={styles.header}>Tic Tac Toe</h1>
                     <div className={styles.box}>
                         <div>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 0")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 1")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 2")}}>Click Me</button>
                         </div>
                         <div>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 3")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 4")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 5")}}>Click Me</button>
                         </div>
                         <div>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
-                            <button className={styles.button}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 6")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 7")}}>Click Me</button>
+                            <button className={styles.button} onPress={()=>{ this.onPress("button 8")}}>Click Me</button>
                         </div>
                     </div>
-                    <h1 className={styles.header}>Tic Tac Toe</h1>
                 </div>
             </>
         );
