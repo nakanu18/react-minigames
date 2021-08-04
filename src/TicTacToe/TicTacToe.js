2c0bbf1aed90785b909a7652dce9db57e46fecde
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-08-03 22:07:25 -0500

    array changes

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -24,19 +24,73 @@ export default class TicTacToe extends Component {
                     <h1 className={styles.header}>Tic Tac Toe</h1>
                     <div className={styles.box}>
                         <div>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 0");}}>{this.state.board[0][0]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 1");}}>{this.state.board[0][1]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 2");}}>{this.state.board[0][2]}</button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 0");
+                                }}>
+                                {this.state.board[0][0]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 1");
+                                }}>
+                                {this.state.board[0][1]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 2");
+                                }}>
+                                {this.state.board[0][2]}
+                            </button>
                         </div>
                         <div>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 3");}}>{this.state.board[0][3]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 4");}}>{this.state.board[0][4]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 5");}}>{this.state.board[0][5]}</button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 3");
+                                }}>
+                                {this.state.board[1][0]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 4");
+                                }}>
+                                {this.state.board[1][1]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 5");
+                                }}>
+                                {this.state.board[1][2]}
+                            </button>
                         </div>
                         <div>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 6");}}>{this.state.board[0][6]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 7");}}>{this.state.board[0][7]}</button>
-                            <button className={styles.button} onPress={()=>{this.onPress("button 8");}}>{this.state.board[0][8]}</button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 6");
+                                }}>
+                                {this.state.board[2][0]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 7");
+                                }}>
+                                {this.state.board[2][1]}
+                            </button>
+                            <button
+                                className={styles.button}
+                                onPress={() => {
+                                    this.onPress("button 8");
+                                }}>
+                                {this.state.board[2][2]}
+                            </button>
                         </div>
                     </div>
                 </div>
