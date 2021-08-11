6ada88a7a4775bf2bebbf54ec2b2bb9e2b61b672
Maxine Causevic <maxcausevic@gmail.com>
2021-08-11 14:42:34 -0500

    more onClicks

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -26,21 +26,21 @@ export default class TicTacToe extends Component {
                         <div>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 0");
                                 }}>
                                 {this.state.board[0][0]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 1");
                                 }}>
                                 {this.state.board[0][1]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 2");
                                 }}>
                                 {this.state.board[0][2]}
@@ -49,21 +49,21 @@ export default class TicTacToe extends Component {
                         <div>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 3");
                                 }}>
                                 {this.state.board[1][0]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 4");
                                 }}>
                                 {this.state.board[1][1]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 5");
                                 }}>
                                 {this.state.board[1][2]}
@@ -72,21 +72,21 @@ export default class TicTacToe extends Component {
                         <div>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 6");
                                 }}>
                                 {this.state.board[2][0]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 7");
                                 }}>
                                 {this.state.board[2][1]}
                             </button>
                             <button
                                 className={styles.button}
-                                onPress={() => {
+                                onClick={() => {
                                     this.onClick("button 8");
                                 }}>
                                 {this.state.board[2][2]}
