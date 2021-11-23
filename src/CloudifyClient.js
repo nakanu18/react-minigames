ad29b85f228dd3df44f4c49e7c48c9c35dcc791f
Maxine Causevic <maxcausevic@gmail.com>
2021-11-23 11:52:51 -0600

    deleted cloudify

--- src/CloudifyClient.js
+++
@@ -1,8 +0,0 @@
-var CloudifyClient = require('cloudify-js').CloudifyClient;
-
-var client = new CloudifyClient({'endpoint' : 'http://cloudify.localhost.com'});
-var logger = require('log4js').getLogger('index.nodejs');
-
-client.blueprints.list(null, function( err, response, body){
-    logger.info('this is body',body);
-});
