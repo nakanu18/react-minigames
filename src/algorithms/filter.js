a070117050d55b4b92753acc80ea95bcb75a9e0f
Alex de Vera <adever2@gmail.com>
2022-03-07 21:13:45 -0600

    filterByFirstLetter: Add comments and examples

--- src/algorithms/filter.js
+++ src/algorithms/filter.js
@@ -1,3 +1,14 @@
+// This function will return an array of strings.  Each string should
+// match the firstLetter given.  If isCaseSensitive is true, then
+// it should only match lower case or upper case.  If isCaseSensitive
+// is false, it should match both
+//
+// Examples:
+// filterByFirstLetter(["apple", "berry", "Avocado"], 'a', true);
+//   should return: ["apple"]
+//
+// filterByFirstLetter(["apple", "berry", "Avocado"], 'b', true);
+//   should return: ["berry"]
 export const filterByFirstLetter = (
     array,
     firstLetter,
--- src/algorithms/filter.test.js
+++ src/algorithms/filter.test.js
@@ -1,6 +1,24 @@
 import { filterByFirstLetter } from "./filter";
 
+// Run this command
+//      yarn test filter
+// This will run all the tests in this file.
+//
+// You'll have to pass each test one by one to solve it all.
 describe("filter tests", () => {
+    // 1: Handle null value parameters
+    //
+    // filterByFirstLetter() should take care of invalid input.  If it does
+    // not, then it will be prone to crashing.
+    //
+    // The parameters sent into filterByFirstLetter() are all invalid in some
+    // way.  Write a condition that checks if these are valid.
+    //
+    // Expected return value: empty array
+    //
+    // Hint: you'll need to understand truthy and falsey
+    // https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+    // https://developer.mozilla.org/en-US/docs/Glossary/Falsy
     it("should return an empty array for invalid parameters", () => {
         expect(filterByFirstLetter(undefined, undefined)).toEqual([]);
         expect(filterByFirstLetter(undefined, null)).toEqual([]);
@@ -11,12 +29,18 @@ describe("filter tests", () => {
         expect(filterByFirstLetter(null, "")).toEqual([]);
     });
 
+    // 2: Here we now have some valid parameters.  But the array is empty
+    // so there's nothing to process.
+    //
+    // Expected return value: empty array
     it("should return an empty array for an empty parameter array", () => {
         expect(filterByFirstLetter([], undefined)).toEqual([]);
         expect(filterByFirstLetter([], null)).toEqual([]);
         expect(filterByFirstLetter([], "")).toEqual([]);
     });
 
+    // 3. Here you'll need to compare the first letter of each string in
+    // the array to the given letter.
     it("should return all words that start with a lowercase a", () => {
         const words = ["apple", "big", "corn", "act", "", "Alpha"];
         const letter = "a";
@@ -24,6 +48,9 @@ describe("filter tests", () => {
         expect(filterByFirstLetter(words, letter)).toEqual(["apple", "act"]);
     });
 
+    // 4. Here you'll need to compare the first letter of each string in
+    // the array to the given letter.  If 3 works, 4 should probably work
+    // without addtional work.
     it("should return all words that start with a uppercase a", () => {
         const words = ["apple", "big", "corn", "act", "", "Alpha"];
         const letter = "A";
@@ -31,6 +58,8 @@ describe("filter tests", () => {
         expect(filterByFirstLetter(words, letter)).toEqual(["Alpha"]);
     });
 
+    // 4. Here you'll need to handle both a and A.  toLowerCase() or
+    // toUpperCase() may be useful.
     it("should return all words that start with any kind of a", () => {
         const words = ["apple", "big", "corn", "act", "", "Alpha"];
         const letter = "A";
